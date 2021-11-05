9a82ca76f8ae32a37cd9fceea00722eb4dec4ca3
sunmengyue <mengyuesun@gmail.com>
2021-11-04 20:06:14 -0400

    add screen frame for the app

---
+++ src/components/MainScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../Styles/Main.css';
+
+const MainScreen = () => {
+  return <div id="main"></div>;
+};
+
+export default MainScreen;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../Styles/Sidebar.css';
+
+const Sidebar = () => {
+  return <aside id="sidebar">Sidebar</aside>;
+};
+
+export default Sidebar;
